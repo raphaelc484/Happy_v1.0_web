79252cdd950946ee1b7a524c6439d85167614d27
Raphael Clemente Miranda <raphaelc484@hotmail.com>
2020-10-17 17:40:53 -0300

    teste

---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+
+import mapMarker from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
+import mapIcon from '../utils/mapIcon';
+
+import api from '../services/api';
+
+interface Orphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
+
+function OrphanagesMap() {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+  useEffect(() => {
+    api.get('/orphanages').then(response => {
+      setOrphanages(response.data);
+    });
+  }, []);
+
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarker} alt="Happy" />
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>São Paulo</strong>
+          <span>São Paulo</span>
+        </footer>
+      </aside>
+      <Map
+        center={[-23.476329, -46.5993016]}
+        zoom={15}
+        style={{
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"></TileLayer> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+
+        {orphanages.map(orphanage => {
+          return (
+            <Marker
+              key={orphanage.id}
+              icon={mapIcon}
+              position={[orphanage.latitude, orphanage.longitude]}
+            >
+              <Popup
+                closeButton={false}
+                minWidth={240}
+                maxHeight={240}
+                className="map-popup"
+              >
+                {orphanage.name}
+                <Link to={`/orphanage/${orphanage.id}`}>
+                  <FiArrowRight size={20} color="#fff" />
+                </Link>
+              </Popup>
+            </Marker>
+          );
+        })}
+      </Map>
+
+      <Link to="/orphanage/create" className="create-orphanage">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+    </div>
+  );
+}
+
+export default OrphanagesMap;
